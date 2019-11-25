1091bfbed80726a5300bfa3e1c5fa3e4cba1340e
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-25 13:12:08 +0300

    create regester form and fucntion to validate

--- pitch.py
+++ pitch.py
@@ -27,6 +27,14 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
