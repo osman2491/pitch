47617c512dd0bcdbe9de95d029ab0c4b622df566
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-26 10:53:36 +0300

    add logout and validation for registration and login

--- pitch/routes.py
+++ pitch/routes.py
@@ -62,6 +62,7 @@ def login():
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+
 @app.route("/logout")
 def logout():
     logout_user()
