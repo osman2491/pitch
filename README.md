5729d56763eaa8f0b0b5da86b310ce71cd067d46
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-27 07:59:47 +0300

    add background image and a README

---
+++ README.md
@@ -0,0 +1,52 @@
+# Pitch
+
+Minute Pitch
+
+# Author
+osman mohamed
+
+# Author Contact details
+Email: osman67239121@gmail.com
+Project Description
+A Flask application that helps In life, you only have 60 seconds to impress someone.This app allows a user to submit an idea pitch.
+
+
+# Set up instructions
+You need to have python3.6 installed, venv and pip
+
+Set up the virtual environment and activate it.
+
+$ python3.6 -m venv virtual
+
+$ source virtual/bin/activate
+
+Install all modules required
+
+$ pip install flask
+
+$ pip install flask-script
+
+# Development
+Want to contribute? Great!
+
+Fork the repo
+Clone the repo in your machine but ensure you have all the necessary modules.(You can find them in the set up instructions above) https://github.com/AliKheirAbdi/pitchit.git
+Create a new branch git branch contributions
+Edit your changes in your branch
+Run the application
+# Known bugs
+No comment box for the pitches.
+
+# Technologies used
+Python3.6 
+Boostrap 4
+
+# BDD
+Input	Output	Behavior
+Click Register	Fill out the reg form	user is created
+Click Login	Enter username and password	Application logs user in
+Submit Pitch	User fills out the form and submits	The new pitch is posted
+Delete Pitch	Modal popup with the delete button	The new pitch is posted
+Click Account	User bio is populated	User can edit their username and email and profile picture.
+
+# License
--- pitch/forms.py
+++ pitch/forms.py
@@ -1,7 +1,7 @@
-from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from pitch.models import User
 
@@ -35,7 +35,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -46,12 +45,18 @@ class UpdateAccountForm(FlaskForm):
 
     def validate_username(self, username):
         if username.data != current_user.username:
-           user = User.query.filter_by(username=username.data).first()
-           if user:
-              raise ValidationError('That username is taken. Please choose a different one.')
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
-           user = User.query.filter_by(email=email.data).first()
-           if user:
-              raise ValidationError('That email is taken. Please choose a different one.')
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
